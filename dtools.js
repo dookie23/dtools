2551d67f1a5c838175b40328e94a4540899d5fdd
Fernando Vilas <fvilas@gradiant.org>
2015-06-11 12:44:30 +0200

    Fixed errors in friendlyDateRepresentation and jsonKeyCharReplacing. Added milisecondsToTime function. Refactored to match airbnb javascript styleguide

--- dtools.js
+++ dtools.js
@@ -1,170 +1,208 @@
 var dtools = {};
 
 (function(exports) {
+  'use strict';
 
-    /**
-     * Complete a number with zeros in the left
-     *
-     * @param  {number} number Number to complete
-     * @param {number} desiredLength Length of the return string
-     * @return {string} String completed with zeros on the left
-     */
-    exports.completeZerosLeft = function(number, desiredLength) {
-            var numberStr = number + "";
-            var result = numberStr;
-            for (var i = 0; i < (desiredLength - numberStr.length); i++) result = "0" + result;
-            return result;
-        },
+  /**
+   * Complete a number with zeros in the left
+   *
+   * @param  {number} number Number to complete
+   * @param {number} desiredLength Length of the return string
+   * @return {string} String completed with zeros on the left
+   */
+  exports.completeZerosLeft = function(number, desiredLength) {
+    var numberStr = number + '';
+    var result = numberStr;
+    for (var i = 0; i < (desiredLength - numberStr.length); i++) result = '0' + result;
+    return result;
+  };
 
-        /**
-         * Generates a random integer in the inout range
-         *
-         * @param {number} min Min. value
-         * @param {number} max Max. value
-         * @return {number} An integer from min to max
-         */
-        exports.randomInt = function(min, max) {
-            return min + Math.round(Math.random() * (max - min));
-        },
+  /**
+   * Generates a random integer in the inout range
+   *
+   * @param {number} min Min. value
+   * @param {number} max Max. value
+   * @return {number} An integer from min to max
+   */
+  exports.randomInt = function(min, max) {
+    return min + Math.round(Math.random() * (max - min));
+  };
 
-        /**
-         *
-         * Generates an array with consecutive numbers from start to start+n-1
-         *
-         * @param {number} start The first item of the array
-         * @param {number} n Array length
-         * @param {Boolean} desc Descendent direction
-         * return {Array.<number>} An array with number items from start to start+n-1. If desc == true, the array will be sort in descendent order.
-         */
-        exports.numberArray = function(start, n, desc) {
-            var r = [];
-            for (var i = start; i < start + n; i++) {
-                if (!desc) r.push(i);
-                else r.unshift(i);
-            }
-            return r;
-        },
+  /**
+   *
+   * Generates an array with consecutive numbers from start to start+n-1
+   *
+   * @param {number} start The first item of the array
+   * @param {number} n Array length
+   * @param {Boolean} desc Descendent direction
+   * return {Array.<number>} An array with number items from start to start+n-1. If desc == true, the array will be sort in descendent order.
+   */
+  exports.numberArray = function(start, n, desc) {
+    var r = [];
+    for (var i = start; i < start + n; i++) {
+      if (!desc) r.push(i);
+      else r.unshift(i);
+    }
 
-        /**
-         *
-         * Formats a date in a human-friendly representation
-         *
-         * @param {string} date A String representing the date to format (accepts the format used in JS Date() function)
-         * return {string} A human-friendly date representation (spanish)
-         */
-        exports.friendlyDateRepresentation = function(date) {
-            var aDay = 3600 * 1000 * 24;
-            var d = new Date(date);
-            var now = new Date();
-            var yesterday = new Date(now.getTime() - aDay);
+    return r;
+  };
 
-            var getTime = function(dateP) {
-                return dateP.toISOString().split("T")[1].substr(0, 5);
-            };
+  /**
+   *
+   * Formats a date in a human-friendly representation
+   *
+   * @param {string} date A String representing the date to format (accepts the format used in JS Date() function)
+   * return {string} A human-friendly date representation (spanish)
+   */
+  exports.friendlyDateRepresentation = function(date) {
+    var aDay = 3600 * 1000 * 24;
+    var d = new Date(date);
+    var now = new Date();
+    var yesterday = new Date(now.getTime() - aDay);
 
-            var getMonth = function(month) {
-                if (month == 1) return "Enero";
-                if (month == 2) return "Febrero";
-                if (month == 3) return "Marzo";
-                if (month == 4) return "Abril";
-                if (month == 5) return "Mayo";
-                if (month == 6) return "Junio";
-                if (month == 7) return "Julio";
-                if (month == 8) return "Agosto";
-                if (month == 9) return "Septiembre";
-                if (month == 10) return "Octubre";
-                if (month == 11) return "Noviembre";
-                if (month == 12) return "Diciembre";
-            };
+    var getTime = function(dateP) {
+      return dateP.toISOString().split('T')[1].substr(0, 5);
+    };
 
-            var getDay = function(dateP) {
-                return dateP.toISOString().split("T")[0].substr(8, 2) + " de " + getMonth(parseInt(dateP.toISOString().split("T")[0].substr(5, 2)));
-            }
+    var getMonth = function(month) {
+      if (month == 1) return 'Enero';
+      if (month == 2) return 'Febrero';
+      if (month == 3) return 'Marzo';
+      if (month == 4) return 'Abril';
+      if (month == 5) return 'Mayo';
+      if (month == 6) return 'Junio';
+      if (month == 7) return 'Julio';
+      if (month == 8) return 'Agosto';
+      if (month == 9) return 'Septiembre';
+      if (month == 10) return 'Octubre';
+      if (month == 11) return 'Noviembre';
+      if (month == 12) return 'Diciembre';
+    };
 
-            if ((now.getTime() - d.getTime()) <= aDay) {
-                if (now.getHours() == d.getHours()) {
-                    var diff = now.getMinutes() - d.getMinutes();
-                    if (diff > 1) return "Hace " + diff + " minutos";
-                    else return "Hace " + diff + " minuto";
-                } else if ((now.getHours() == (d.getHours() + 1)) && (d.getMinutes() > now.getMinutes())) return "Hace " + (now.getMinutes() + (60 - d.getMinutes())) + " minutos";
-                else {
-                    var diff = now.getHours() - d.getHours();
-                    if (diff < 1) diff = 24 + diff;
-                    if (diff > 1) return "Hace " + diff + " horas"
-                    else return "Hace " + diff + " hora";
-                }
-            } else if (yesterday.getDate() == d.getDate()) return "Ayer, a las " + getTime(d);
-            else return getDay(d);
-        },
+    var getDay = function(dateP) {
+      return dateP.toISOString().split('T')[0].substr(8, 2) + ' de ' + getMonth(parseInt(dateP.toISOString().split('T')[0].substr(5, 2)));
+    };
 
-        /**
-         * Replace substrings in JSON keys
-         *
-         * @param {Object} obj JSON to change
-         * @param {string} searchValue The value, or regular expression, that will be replaced by the new value
-         * @param {string} newValue The value to replace the searchvalue with
-         * @param {boolean} recursive If true, the replace is excuted in a recursive way, else only first-level keys will be replaced
-         * @return {Object} A new JSON Object, where the specified key(s) has been replaced by the new value
-         */
-        exports.jsonKeyCharReplacing = function(obj, searchValue, newValue, recursive) {
+    if ((now.getTime() - d.getTime()) <= aDay) {
 
-            if (recursive == undefined) recursive = true;
+      if (now.getHours() == d.getHours()) {
+        var diff = now.getMinutes() - d.getMinutes();
 
-            if (obj instanceof Array) {
-                for (var i = 0; i < obj.length; i++) {
-                    obj[i] = exports.jsonKeyCharReplacing(obj[i], searchValue, newValue, recursive);
-                }
-            } else {
-                for (var key in obj) {
+        if (diff > 1) return 'Hace ' + diff + ' minutos';
+        else return 'Hace ' + diff + ' minuto';
 
-                    if (obj.hasOwnProperty(key)) {
-                        var newKey = key.replace(searchValue, newValue);
-                        if (newKey !== key) {
-                            if (recursive) obj[newKey] = exports.jsonKeyCharReplacing(obj[key], searchValue, newValue, recursive);
-                            else obj[newKey] = obj[key];
-                            delete(obj[key]);
-                        }
-                    }
-                }
-            }
+      } else {
 
-            return obj;
-        },
-
-        /**
-         * Remove 'blank elements' in a string array
-         *
-         * @param {string} stringArray An array of string
-         * @param {string=} blankString An string as blank element
-         * @return {Array.<string>} An array without 'blank elements'
-         */
-        exports.cleanArray = function(stringArray, blankString) {
-            var x = [];
-            if (!blankString) blankString = '';
-
-            for (var i = 0; i < stringArray.length; i++) {
-                if (stringArray[i].replace(blankString, '').length > 0) x.push(stringArray[i]);
-            }
-
-            return x;
-        },
-
-
-        /**
-         * Look for one string in one string array
-         *
-         * @param {string} searchString String to find
-         * @param {Array.<string>} stringArray String array to iterate
-         * @param {boolean} stricted If true, the string must be equal one array element.
-         *  If false, the string must be included in one array element
-         * @return {boolean} True if the input string is included 
-         */
-        exports.findStringInArray = function(searchString, stringArray, stricted) {
-            for (var i = 0; i < stringArray.length; i++) {
-                if (stringArray[i].indexOf(searchString) != -1)
-                    if (!stricted) return true;
-                    else if (stringArray[i].length === searchString.length) return true;
-            }
-            return false;
+        if ((now.getHours() == (d.getHours() + 1)) && (d.getMinutes() > now.getMinutes())) {
+          return 'Hace ' + (now.getMinutes() + (60 - d.getMinutes())) + ' minutos';
+        } else {
+          var diff = now.getHours() - d.getHours();
+          if (diff < 1) diff = 24 + diff;
+          if (diff > 1) return 'Hace ' + diff + ' horas'
+          else return 'Hace ' + diff + ' hora';
         }
+
+      }
+
+    } else if (yesterday.toDateString() == d.toDateString()) return 'Ayer, a las ' + getTime(d);
+    else return getDay(d);
+  };
+
+  /**
+   * Replace substrings in JSON keys
+   *
+   * @param {Object} obj JSON to change
+   * @param {string} searchValue The value, or regular expression, that will be replaced by the new value
+   * @param {string} newValue The value to replace the searchvalue with
+   * @param {boolean} recursive If true, the replace is excuted in a recursive way, else only first-level keys will be replaced
+   * @return {Object} A new JSON Object, where the specified key(s) has been replaced by the new value
+   */
+  exports.jsonKeyCharReplacing = function(obj, searchValue, newValue, recursive) {
+
+    if (recursive == undefined) recursive = true;
+
+    if (Object.prototype.toString.call(obj) === '[object Array]') {
+      for (var i = 0; i < obj.length; i++) {
+        obj[i] = exports.jsonKeyCharReplacing(obj[i], searchValue, newValue, recursive);
+      }
+    } else {
+      for (var key in obj) {
+
+        if (obj.hasOwnProperty(key)) {
+          var newKey = key.split(searchValue).join(newValue);
+
+          var type = Object.prototype.toString.call(obj[key]);
+          if (newKey !== key) {
+
+            if (recursive) obj[newKey] = exports.jsonKeyCharReplacing(obj[key], searchValue, newValue, recursive);
+            else obj[newKey] = obj[key];
+            delete(obj[key]);
+
+          } else if (type === '[object Object]' || type === '[object Array]') {
+            obj[key] = exports.jsonKeyCharReplacing(obj[key], searchValue, newValue, recursive);
+          }
+
+        }
+      }
+    }
+
+    return obj;
+  };
+
+  /**
+   * Remove 'blank elements' in a string array
+   *
+   * @param {string} stringArray An array of string
+   * @param {string=} blankString An string as blank element
+   * @return {Array.<string>} An array without 'blank elements'
+   */
+  exports.cleanArray = function(stringArray, blankString) {
+    var x = [];
+    if (!blankString) blankString = '';
+
+    for (var i = 0; i < stringArray.length; i++) {
+      if (stringArray[i].replace(blankString, '').length > 0) x.push(stringArray[i]);
+    }
+
+    return x;
+  };
+
+  /**
+   * Look for one string in one string array
+   *
+   * @param {string} searchString String to find
+   * @param {Array.<string>} stringArray String array to iterate
+   * @param {boolean} stricted If true, the string must be equal one array element.
+   *  If false, the string must be included in one array element
+   * @return {boolean} True if the input string is included
+   */
+  exports.findStringInArray = function(searchString, stringArray, stricted) {
+    for (var i = 0; i < stringArray.length; i++) {
+      if (stringArray[i].indexOf(searchString) !== -1) {
+        if (!stricted) return true;
+        else if (stringArray[i].length === searchString.length) return true;
+      }
+    }
+
+    return false;
+  };
+
+  /**
+   * Transforms miliseconds into time with format "hours minutes seconds"
+   * @param  {Integer} miliseconds Time in miliseconds
+   * @return {String}             Time formated to string
+   */
+  exports.milisecondsToTime = function(miliseconds) {
+    var hours = parseInt(((miliseconds / 60000) / 60).toFixed(0));
+    var minutes = parseInt(Math.floor(miliseconds / 60000) % 60);
+    var seconds = parseInt(((miliseconds % 60000) / 1000).toFixed(0));
+
+    var result = String();
+    if (hours) result = hours + 'h ';
+    if (hours || minutes) result += minutes + 'm ';
+    if (hours || minutes || seconds) result += seconds + 's';
+
+    return result;
+  };
+
 }(typeof exports === 'undefined' ? this.dtools = {} : exports));
--- test/index.js
+++ test/index.js
@@ -1,187 +1,194 @@
 var should = require('chai').should(),
-    dtools = require('../dtools');
+  dtools = require('../dtools');
 
 describe('#completeZerosLeft', function() {
-    it('converts 2 in 4 characters string', function() {
-        out = dtools.completeZerosLeft(2, 4)
-        out.should.be.a('string');
-        out.should.equal('0002');
-        out.should.have.length(4);
-    });
+  it('converts 2 in 4 characters string', function() {
+    out = dtools.completeZerosLeft(2, 4)
+    out.should.be.a('string');
+    out.should.equal('0002');
+    out.should.have.length(4);
+  });
 
-    it('converts 24 in 3 characters string', function() {
-        dtools.completeZerosLeft(24, 3).should.equal('024');
-    });
+  it('converts 24 in 3 characters string', function() {
+    dtools.completeZerosLeft(24, 3).should.equal('024');
+  });
 });
-
 
 describe('randomInt', function() {
-    it('generates an integer in range [0,10]', function() {
-        out = dtools.randomInt(0, 10);
-        out.should.least(0);
-        out.should.most(10);
-    });
+  it('generates an integer in range [0,10]', function() {
+    out = dtools.randomInt(0, 10);
+    out.should.least(0);
+    out.should.most(10);
+  });
 
-    it('generates an integer in range [2,4]', function() {
-        out = dtools.randomInt(2, 4);
-        out.should.least(2);
-        out.should.most(4);
-    });
+  it('generates an integer in range [2,4]', function() {
+    out = dtools.randomInt(2, 4);
+    out.should.least(2);
+    out.should.most(4);
+  });
 });
 
-
 describe('numberArray', function() {
-    it('generates the array [2013,2014,2015]', function() {
-        out = dtools.numberArray(2013, 3);
-        out[0].should.equal(2013);
-        out[1].should.equal(2014);
-        out[2].should.equal(2015);
-    });
+  it('generates the array [2013,2014,2015]', function() {
+    out = dtools.numberArray(2013, 3);
+    out[0].should.equal(2013);
+    out[1].should.equal(2014);
+    out[2].should.equal(2015);
+  });
 
-    it('generates the array [2015,2014,2013]', function() {
-        out = dtools.numberArray(2013, 3, true);
-        out[0].should.equal(2015);
-        out[1].should.equal(2014);
-        out[2].should.equal(2013);
-    });
+  it('generates the array [2015,2014,2013]', function() {
+    out = dtools.numberArray(2013, 3, true);
+    out[0].should.equal(2015);
+    out[1].should.equal(2014);
+    out[2].should.equal(2013);
+  });
 
-    it('generates an array of 100 consecutive numbers', function() {
-        out = dtools.numberArray(1, 100);
-        out.length.should.equal(100);
-    });
+  it('generates an array of 100 consecutive numbers', function() {
+    out = dtools.numberArray(1, 100);
+    out.length.should.equal(100);
+  });
 });
 
 describe('friendlyDateRepresentation', function() {
-    it('today - minutes', function() {
-        var d1 = new Date();
-        var d2 = new Date(d1.getTime() - (60 * 1000 * 30));
-        out = dtools.friendlyDateRepresentation(d2.toISOString());
-        out.should.contain("Hace 30 minutos");
-    });
+  it('today - minutes', function() {
+    var d1 = new Date();
+    var d2 = new Date(d1.getTime() - (60 * 1000 * 30));
+    out = dtools.friendlyDateRepresentation(d2.toISOString());
+    out.should.contain('Hace 30 minutos');
+  });
 
-    it('today - hours', function() {
-        var d1 = new Date();
-        var d2 = new Date(d1.getTime() - (60 * 1000 * 60));
-        out = dtools.friendlyDateRepresentation(d2.toISOString());
-        out.should.contain("Hace 1 hora");
-    });
+  it('today - hours', function() {
+    var d1 = new Date();
+    var d2 = new Date(d1.getTime() - (60 * 1000 * 60));
+    out = dtools.friendlyDateRepresentation(d2.toISOString());
+    out.should.contain('Hace 1 hora');
+  });
 
-    it('yesterday', function() {
-        out = dtools.friendlyDateRepresentation(new Date(new Date().getTime() - 3600 * 1000 * 24).toString());
-        out.should.contain("Ayer");
-    });
+  it('yesterday', function() {
+    out = dtools.friendlyDateRepresentation(new Date(new Date().getTime() - 3600 * 1000 * 24).toString());
+    out.should.contain('Ayer');
+  });
 
-    it('yesterday (negation)', function() {
-        out = dtools.friendlyDateRepresentation(new Date(new Date().getTime() - 3600 * 1000 * 24 * 3).toString());
-        out.should.not.contain("Ayer");
-    });
+  it('yesterday (negation)', function() {
+    out = dtools.friendlyDateRepresentation(new Date(new Date().getTime() - 3600 * 1000 * 24 * 3).toString());
+    out.should.not.contain('Ayer');
+  });
 
-    it('a day', function() {
-        out = dtools.friendlyDateRepresentation("2001-01-10");
-        out.should.contain("10 de Enero");
-    });
+  it('a day', function() {
+    out = dtools.friendlyDateRepresentation('2001-01-10');
+    out.should.contain('10 de Enero');
+  });
 
-    it('other day', function() {
-        out = dtools.friendlyDateRepresentation("2001-01-24T23:15:00.000Z");
-        out.should.contain("24 de Enero");
-    });
+  it('other day', function() {
+    out = dtools.friendlyDateRepresentation('2001-01-24T23:15:00.000Z');
+    out.should.contain('24 de Enero');
+  });
 
 });
-
 
 describe('jsonKeyCharReplacing', function() {
 
-    var testJSON = {};
+  var testJSON = {};
 
-    beforeEach(function(done) {
-        testJSON = {
-            "test.con": [],
-            "test2.con": {
-                "test3.con": [{
-                    "test4.con": "hello"
-                }],
-                "test5.con": "hello.man"
-            },
-            "test6.con": "hi.",
-            "test7con": {
-                "test8con": [{
-                    "test9.con": "hello"
-                }],
-                "test.10.con": "hello.man"
-            }
-        };
-        done();
-    });
+  beforeEach(function(done) {
+    testJSON = {
+      'test.con': [],
+      'test2.con': {
+        'test3.con': [{
+          'test4.con': 'hello'
+        }],
+        'test5.con': 'hello.man'
+      },
+      'test6.con': 'hi.',
+      'test7con': {
+        'test8con': [{
+          'test9.con': 'hello'
+        }],
+        'test.10.con': 'hello.man'
+      }
+    };
+    done();
+  });
 
+  it('replace - first level keys', function() {
+    out = dtools.jsonKeyCharReplacing(testJSON, '\.', '_', false);
+    out.test2_con['test5.con'].should.be.equal('hello.man');
+    out.test6_con.should.be.equal('hi.');
+  });
 
-    it('replace - first level keys', function() {
-        out = dtools.jsonKeyCharReplacing(testJSON, "\\.", "_", false);
-        out.test2_con["test5.con"].should.be.equal("hello.man");
-        out.test6_con.should.be.equal("hi.");
-    });
-
-    it('replace - all keys', function() {
-        out = dtools.jsonKeyCharReplacing(testJSON, "\\.", "_", true);
-        out.test2_con["test5_con"].should.be.equal("hello.man");
-        out.test2_con.test3_con[0]["test4_con"].should.be.equal("hello");
-        out.test7con.test8con[0]["test9_con"].should.be.equal("hello");
-        out.test7con.test_10_con.should.be.equal("hello.man");
-    });
+  it('replace - all keys', function() {
+    out = dtools.jsonKeyCharReplacing(testJSON, '\.', '_', true);
+    out.test2_con['test5_con'].should.be.equal('hello.man');
+    out.test2_con.test3_con[0]['test4_con'].should.be.equal('hello');
+    out.test7con.test8con[0]['test9_con'].should.be.equal('hello');
+    out.test7con.test_10_con.should.be.equal('hello.man');
+  });
 });
-
-
 
 describe('cleanArray', function() {
 
-    var testArray = [];
+  var testArray = [];
 
-    beforeEach(function(done) {
-        testArray = ['Local','','','Visitante','','','',];
-        done();
-    });
+  beforeEach(function(done) {
+    testArray = ['Local', '', '', 'Visitante', '', '', ''];
+    done();
+  });
 
+  it('clean array basic', function() {
+    out = dtools.cleanArray(testArray);
+    out.length.should.be.equal(2);
+    out[0].should.be.equal('Local');
+    out[1].should.be.equal('Visitante');
+  });
 
-    it('clean array basic', function() {
-        out = dtools.cleanArray(testArray);
-        out.length.should.be.equal(2);
-        out[0].should.be.equal("Local");
-        out[1].should.be.equal("Visitante");
-    });
-
-        it('clean array custom blank element', function() {
-        out = dtools.cleanArray(testArray, 'Local');
-        out.length.should.be.equal(1);
-        out[0].should.be.equal("Visitante");
-    });
+  it('clean array custom blank element', function() {
+    out = dtools.cleanArray(testArray, 'Local');
+    out.length.should.be.equal(1);
+    out[0].should.be.equal('Visitante');
+  });
 
 });
 
-
-
-
 describe('findStringInArray', function() {
 
-    var testArray = [];
+  var testArray = [];
 
-    beforeEach(function(done) {
-        testArray = ['Local','','','Visitante y más','','','',];
-        done();
-    });
+  beforeEach(function(done) {
+    testArray = ['Local', '', '', 'Visitante y más', '', '', ''];
+    done();
+  });
 
+  it('findStringInArray (no stricted)', function() {
+    out = dtools.findStringInArray('Visitante', testArray, false);
+    out.should.be.equal(true);
+    out = dtools.findStringInArray('Local', testArray, false);
+    out.should.be.equal(true);
+  });
 
-    it('findStringInArray (no stricted)', function() {
-        out = dtools.findStringInArray("Visitante", testArray, false);
-        out.should.be.equal(true);
-        out = dtools.findStringInArray("Local", testArray, false);
-        out.should.be.equal(true);
-    });
+  it('findStringInArray (stricted)', function() {
+    out = dtools.findStringInArray('Visitante', testArray, true);
+    out.should.be.equal(false);
+    out = dtools.findStringInArray('Local', testArray, true);
+    out.should.be.equal(true);
+  });
 
-    it('findStringInArray (stricted)', function() {
-        out = dtools.findStringInArray("Visitante", testArray, true);
-        out.should.be.equal(false);
-        out = dtools.findStringInArray("Local", testArray, true);
-        out.should.be.equal(true);
-    });
+});
 
+describe('milisecondsToTime', function() {
+
+  it('seconds', function() {
+    out = dtools.milisecondsToTime(3012);
+    out.should.be.equal('3s');
+  });
+
+  it('minutes', function() {
+    out = dtools.milisecondsToTime(123012);
+    out.should.be.equal('2m 3s');
+  });
+
+  it('hours', function() {
+    out = dtools.milisecondsToTime(12123012);
+    out.should.be.equal('3h 22m 3s');
+  });
 
 });
